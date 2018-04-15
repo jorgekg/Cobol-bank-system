adc188a4f0f89307fc1123114c82efd191c05982
jorge <jorgekg3@gmail.com>
2018-04-15 16:48:09 -0300

    cOMMIT AJUSTADO PROBLEMA AO EDITAR UM ARQUIVO.

--- peoplesave.cob
+++ peoplesave.cob
@@ -4,7 +4,6 @@
        ENVIRONMENT DIVISION.
            INPUT-OUTPUT SECTION.
              FILE-CONTROL.
-             SELECT PEOPLE ASSIGN TO 'storage/people.data'.
              SELECT PEOPLE-RW ASSIGN TO 'storage/people.data'
              ORGANIZATION IS INDEXED
              ACCESS IS RANDOM
@@ -13,17 +12,6 @@
 
        DATA DIVISION.
            FILE SECTION.
-           FD PEOPLE.
-           01 PEOPLE-FILE.
-               05 PEOPLE-ID PIC 9(10).
-               05 PEOPLE-NAME PIC A(255).
-               05 PEOPLE-BIRTH PIC A(8).
-               05 PEOPLE-CITY PIC A(255).
-               05 PEOPLE-NEIGHBORHOOD PIC A(255).
-               05 PEOPLE-STREET PIC A(255).
-               05 PEOPLE-NUMBER PIC 9(10).
-               05 PEOPLE-CEP PIC 9(10).
-               05 PEOPLE-SIGNATURE PIC A(10).
            FD PEOPLE-RW.
            01 PEOPLE-RW-FILE.
                05 PEOPLE-RW-ID PIC 9(10).
@@ -48,7 +36,7 @@
                05 WS-PEOPLE-FILE-IN-CEP PIC 9(10).
                05 WS-PEOPLE-FILE-IN-SIGNATURE PIC A(10).
            01 WS-EOF PIC A(1).
-           01 WS-EXSIST PIC A(1) value 'N'.
+           01 WS-EXSIST PIC A(1).
            01 FS PIC 9(10) USAGE NATIONAL.
 
            LINKAGE SECTION.
@@ -65,38 +53,21 @@
 
 
        PROCEDURE DIVISION USING WS-PEOPLE-IN.
-           OPEN INPUT PEOPLE.
-           PERFORM UNTIL WS-EOF='Y'
-               READ PEOPLE INTO WS-PEOPLE-FILE-IN
-               AT END MOVE 'Y' TO WS-EOF
-                   not at end
-                   IF WS-PEOPLE-IN-ID = WS-PEOPLE-FILE-IN-ID THEN
-                       MOVE 'Y' TO WS-EXSIST
-                   END-IF
-                   END-READ
-               END-PERFORM.
-           CLOSE PEOPLE.
-           MOVE WS-PEOPLE-IN TO PEOPLE-FILE.
-           IF WS-EXSIST = 'N' THEN
-               OPEN EXTEND PEOPLE
-                   WRITE PEOPLE-FILE
+           OPEN I-O PEOPLE-RW
+               MOVE WS-PEOPLE-IN-ID TO PEOPLE-RW-ID
+               READ PEOPLE-RW
+                   KEY IS PEOPLE-RW-ID
+                   INVALID KEY
+                   WRITE PEOPLE-RW-FILE
                    END-WRITE
-               CLOSE PEOPLE
-               DISPLAY 'Cliente salvo com sucesso: 'WS-PEOPLE-IN-ID
-           ELSE
-               OPEN I-O PEOPLE-RW
-                   MOVE WS-PEOPLE-IN-ID TO PEOPLE-RW-ID
-                   DISPLAY PEOPLE-RW-ID
-                   READ PEOPLE-RW
-                      KEY IS PEOPLE-RW-ID
-                      INVALID KEY DISPLAY 'KEY IS NOT EXISTING'
-                      NOT INVALID KEY DISPLAY PEOPLE-RW-NAME
-                   END-READ
-                   MOVE 'TSALKDÇLAKSDÇLAKSLÇKD' TO PEOPLE-RW-NAME
-                   DISPLAY PEOPLE-RW-FILE
+                   DISPLAY 'GRAVAR'
+                   NOT INVALID KEY
+                   MOVE WS-PEOPLE-IN-NAME TO PEOPLE-RW-NAME
                    REWRITE PEOPLE-RW-FILE
                    END-REWRITE
-               CLOSE PEOPLE-RW
-               DISPLAY 'Cliente atualizado com sucesso: 'WS-PEOPLE-IN-ID
-           END-IF
+                   DISPLAY 'ATUALIZAR'
+               END-READ
+               DISPLAY PEOPLE-RW-FILE
+
+           CLOSE PEOPLE-RW.
            goback.
